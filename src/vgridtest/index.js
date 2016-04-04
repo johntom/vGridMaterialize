040a28a8d3a158304acfa9737cd0fc2062d3a75a
johntom <jrt@gtz.com>
2016-04-04 13:26:07 -0400

    :ver 1.0.2

--- src/vgridtest/index.js
+++ src/vgridtest/index.js
@@ -44,7 +44,7 @@ export class Vgridtest {
     myCurrentEntity2 = {};
     gridData = [];
     grid1 = {};
-    grid2 ={};
+    grid2 = {};
     grid3 = {};
  
     
@@ -62,35 +62,39 @@ export class Vgridtest {
         //this if just for giving us some data this.dummyDataGenerator.prototype
         this.dummyDataGenerator = dummyDataGenerator;
         this.dummyDataGenerator.generateData(100, (data) => {
-            
+
             this.newdata = [];
-            let ct=0;
+            let ct = 0;
             lodash.forEach(data, (key, value) => {
                 if (typeof key === 'object') {
                     //    return new Person(key);
                     let p = new Person(key);
                     this.newdata.push(p);
-                    if (ct===0)   this.myCurrentEntity = p;
-                     ct ++;
+                    if (ct === 0) this.myCurrentEntity = p;
+                    ct++;
                     return p
                     //lodash.assignIn({ 'link': 'www.gtz' }, key);  
                 }
                 //return new Person(key);
                 //newdata return key;
             });
-            
+
+            // grid1
             this.myCollection = this.newdata;//data;
             this.collectionLength = this.myCollection.length;
+            // grid2 (modal1)
             this.myCollection1 = this.newdata;
             this.collectionLength1 = this.myCollection1.length;
-            
+            // grid3 (modal2)
+            this.myCollection2 = this.newdata;
+            this.collectionLength2 = this.myCollection2.length;
         });
 
     }
 
     popup() {
         $('#modal1').openModal();
-         this.grid1.ctx.redrawGrid();
+        this.grid2.ctx.redrawGrid();
     }
 
 
@@ -106,56 +110,54 @@ export class Vgridtest {
 
         this.myCurrentEntity2 = {};
         $('#modal2').openModal();
-        this.grid2.ctx.redrawGrid();
+        this.grid3.ctx.redrawGrid();
     }
   
     //http://tutaurelia.net/2016/01/10/export-data-to-excel-from-an-aurelia-web-application/
-    exportContactsnofilter() {
-        this.ctr = 1;
-        this.csvContent = "name, country, email ,color\n";
-        this.myCollection.forEach(contact => {
-            this.ctr++;
-            this.csvContent = this.csvContent + '"' + contact.name + '","' + contact.country + '","' + contact.email + '","' + contact.color + '"\n';
-        });
+    // exportContactsnofilter() {
+    //     this.ctr = 1;
+    //     this.csvContent = "name, country, email ,color\n";
+    //     this.myCollection.forEach(contact => {
+    //         this.ctr++;
+    //         this.csvContent = this.csvContent + '"' + contact.name + '","' + contact.country + '","' + contact.email + '","' + contact.color + '"\n';
+    //     });
 
-        this.csvContent = this.csvContent + "COUNT,=ROWS(A2:A" + this.ctr + ") \n";
-        //this.ctr = 1;
-        this.export(this.csvContent);
-    }
-     exportContacts() {
+    //     this.csvContent = this.csvContent + "COUNT,=ROWS(A2:A" + this.ctr + ") \n";
+    //     //this.ctr = 1;
+    //     this.export(this.csvContent);
+    // }
+    exportContacts() {
         this.ctr = 1;
         this.csvContent = "name, country, email ,color\n";
-        // let ds = this.myGrid.ctx.getGridRows() ;
-        //let ds = this.myGrid.ctx.getGridRows() ;
         let row = {};
-        let ds = this.myCollection.ctx.getGridRows();
-        //this.myCollection.forEach(contact => {
+        let ds = this.grid2.ctx.getGridRows(); //this.myCollection1.ctx.getGridRows();
         ds.forEach(contact => {
             this.ctr++;
-            row = this.myCollection[contact];
-            //  this.csvContent =row + '"'+row + '","' + row+ '","' + row + '","' +row+ '"\n';
+            row = this.myCollection1[contact];
             this.csvContent = this.csvContent + '"' + row.name + '","' + row.country + '","' + row.email + '","' + row.color + '"\n';
-
-            // this.csvContent = this.csvContent + '"'+contact.name + '","' + contact.country + '","' + contact.email + '","' + contact.color + '"\n';
         });
-
         this.csvContent = this.csvContent + "COUNT,=ROWS(A2:A" + this.ctr + ") \n";
-        //this.ctr = 1;
         this.export(this.csvContent);
-    
+
     }
     exportContacts2() {
         this.ctr = 1;
         this.csvContent = "name, country, email ,color\n";
-        this.myCollection2.forEach(contact => {
-            this.ctr++;
-            this.csvContent = this.csvContent + '"' + contact.name + '","' + contact.country + '","' + contact.email + '","' + contact.color + '"\n';
-        });
+        let row = {};
+        let ds = this.grid3.ctx.getGridRows();
 
-        this.csvContent = this.csvContent + "COUNT,=ROWS(A2:A" + this.ctr + ") \n";
-        //this.ctr = 1;
+        ds.forEach(contact => {
+            this.ctr++;
+            row = this.myCollection2[contact];
+            this.csvContent = this.csvContent + '"' + row.name + '","' + row.country + '","' + row.email + '","' + row.color + '"\n';
+
+        });
+        this.csvContent = this.csvContent2 + "COUNT,=ROWS(A2:A" + this.ctr + ") \n";
+
         this.export(this.csvContent);
+
     }
+
     export(text) {
         var pom = document.createElement('a');
         pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -165,6 +167,7 @@ export class Vgridtest {
         pom.click();
         document.body.removeChild(pom);
     }
+    
 
 }
 
